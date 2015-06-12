6dc542f0c8885dd98c1270520f0a59c2049e5081
Jignesh Kakadiya <jignesh.kakadiya@inmobi.com>
2015-06-12 19:46:25 +0530

    use dummy context to render the content

--- dist/app.js
+++ dist/app.js
@@ -6,89 +6,72 @@
 
     var slate = {
         render: function (canvas) {
-            var options = arguments[1] === undefined ? {} : arguments[1];
+            var options = arguments[1] === undefined ? { element: element, bgColor: bgColor, textColor: textColor, font: font, delay: delay } : arguments[1];
+            return (function () {
+                var ctx = canvas.getContext('2d');
+                var w = canvas.width;
+                var h = canvas.height;
+                var x = w / 2;
+                var y = h / 2;
+                var bgColor = bgColor || '#018790';
+                var textColor = textColor || '#fff';
+                var font = font || '100 30px/30px "Times New Roman", Times, serif';
+                var delay = delay || 300;
+                var fontHeight = 30;
 
-            var ctx = canvas.getContext('2d');
-            var w = canvas.width;
-            var h = canvas.height;
-            var x = canvas.width / 2;
-            var y = canvas.height / 2;
-            var bgColor = options.bgColor || 'yellow';
-            var textColor = options.textColor || 'red';
-            var font = 'bold 30px Helvetica';
-            var delay = 300;
-
-            var gif = new GIF({
-                workers: 2,
-                quality: 10,
-                width: 400,
-                height: 400
-            });
-
-            ctx.fillStyle = bgColor;
-            ctx.font = font;
-            ctx.textAlign = 'center';
-            ctx.fillRect(0, 0, w, h);
-
-            var text = options.text || 'hello world';
-            var splitArray = text.split(' ');
-
-            for (var i = 0, l = splitArray.length; i < l; i++) {
-                ctx.fillStyle = textColor;
-                ctx.fillText(splitArray[i], x, y);
-                gif.addFrame(ctx, { copy: true, delay: delay });
+                var gif = new GIF({
+                    workers: 2,
+                    quality: 10,
+                    width: 400,
+                    height: 400
+                });
 
                 ctx.fillStyle = bgColor;
+                ctx.font = font;
+                ctx.textAlign = 'center';
                 ctx.fillRect(0, 0, w, h);
+
+                var text = options.text || 'hello world';
+                var splitArray = text.split(' ');
+
+                var scale = 2;
+                var ocanvas = document.createElement('canvas');
+                var octx = ocanvas.getContext('2d');
+                var ow = w * scale;
+                var oh = h * scale;
+                ocanvas.width = ow;
+                ocanvas.height = oh;
+                octx.font = fontHeight * scale + 'px Times New Roman, Times, serif';
+
+                octx.textAlign = 'center';
                 gif.addFrame(ctx, { copy: true, delay: delay });
-            }
 
-            gif.on('finished', function (blob) {
-                window.open(URL.createObjectURL(blob));
-            });
+                for (var i = 0, l = splitArray.length; i < l; i++) {
+                    octx.fillStyle = bgColor;
+                    octx.fillRect(0, 0, ow, oh);
+                    octx.fillStyle = textColor;
+                    octx.fillText(splitArray[i], ow / 2, oh / 2);
 
-            gif.render();
+                    ctx.drawImage(ocanvas, 0, 0, w, h);
+                    gif.addFrame(ctx, { copy: true, delay: delay });
+
+                    octx.fillStyle = bgColor;
+                    octx.fillRect(0, 0, ow, oh);
+
+                    ctx.drawImage(ocanvas, 0, 0, w, h);
+                    gif.addFrame(ctx, { copy: true, delay: delay });
+                }
+
+                gif.on('finished', function (blob) {
+                    window.open(URL.createObjectURL(blob));
+                });
+
+                gif.render();
+            })();
         }
     };
 
     var canvas = document.getElementById('canvas');
     slate.render(canvas, { element: '.result', text: 'There is another world out' });
-
-    // let context = canvas.getContext('2d');
-    // context.font = "bold 70px Helvetica"
-    // context.textAlign = 'center'
-    // context.textBaseline = 'middle'
-    // context.lineWidth = 3;
-    // context.fillText("Hello", 75, 75);
-
-    // let gif = new GIF({
-    //   workers: 2,
-    //   quality: 10,
-    //   width: 150,
-    //   height: 150
-    // });
-
-    // // add an image element
-    // // gif.addFrame(imageElement);
-
-    // gif.addFrame context {copy: true, delay: 200}
-
-    // context.font = "bold 70px Helvetica"
-    // context.textAlign = 'center'
-    // context.textBaseline = 'middle'
-    // context.lineWidth = 3;
-    // context.fillStyle = "rgb(255,255,255)";
-    // context.fillRect(0, 0, 150, 150)
-    // context.fillText("Hello", 75, 75)
-
-    // gif.addFrame(context, {copy: true, delay: 200});
-    // // // or copy the pixels from a canvas context
-    // // gif.addFrame(ctx, {copy: true});
-
-    // gif.on('finished', function(blob) {
-    //   window.open(URL.createObjectURL(blob));
-    // });
-
-    // gif.render();
 
 }));
--- src/js/slate.js
+++ src/js/slate.js
@@ -1,14 +1,15 @@
 export default {
-    render(canvas, options = {}) {
+    render(canvas, options = { element, bgColor, textColor, font, delay }) {
         let ctx = canvas.getContext('2d');
         let w = canvas.width;
         let h = canvas.height;
-        let x = canvas.width / 2;
-        let y = canvas.height / 2;
-        let bgColor = options.bgColor || 'yellow';
-        let textColor = options.textColor || 'red';
-        let font = 'bold 30px Helvetica';
-        let delay = 300;
+        let x = w / 2;
+        let y = h / 2;
+        let bgColor = bgColor || '#018790';
+        let textColor = textColor || '#fff';
+        let font = font || '100 30px/30px "Times New Roman", Times, serif';
+        let delay = delay || 200;
+        let fontHeight = 30;
 
         let gif = new GIF({
           workers: 2,
@@ -22,18 +23,35 @@ export default {
         ctx.textAlign = 'center';
         ctx.fillRect(0, 0, w, h);
 
-        var text = options.text || 'hello world';
+        let text = options.text || 'hello world';
         let splitArray = text.split(' ');
 
+        let scale = 2;
+        let ocanvas = document.createElement('canvas');
+        let octx = ocanvas.getContext('2d');
+        let ow = w * scale;
+        let oh = h * scale;
+        ocanvas.width = ow;
+        ocanvas.height = oh;
+        octx.font = (fontHeight * scale) + "px Times New Roman, Times, serif";
+
+        octx.textAlign = 'center';
+        gif.addFrame(ctx, {copy: true, delay: delay});
 
         for (let i = 0, l = splitArray.length; i < l; i++) {
-            ctx.fillStyle = textColor;
-            ctx.fillText(splitArray[i], x, y);
-            gif.addFrame(ctx, {copy: true, delay: delay})
+            octx.fillStyle = bgColor;
+            octx.fillRect(0, 0, ow, oh);
+            octx.fillStyle = textColor;
+            octx.fillText(splitArray[i], ow / 2, oh / 2);
 
-            ctx.fillStyle = bgColor;
-            ctx.fillRect(0, 0, w, h);
-            gif.addFrame(ctx, {copy: true, delay: delay})
+            ctx.drawImage(ocanvas, 0, 0, w, h);
+            gif.addFrame(ctx, {copy: true, delay: delay});
+
+            octx.fillStyle = bgColor;
+            octx.fillRect(0, 0, ow, oh);
+
+            ctx.drawImage(ocanvas, 0, 0, w, h);
+            gif.addFrame(ctx, {copy: true, delay: delay});
         }
 
         gif.on('finished', function(blob) {
